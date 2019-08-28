12f61109af7126de49eecacd9080c946aae33b22
JeyaBalaji Samuthiravelu <jeyabalaji95@gmail.com>
2019-08-27 22:48:15 -0300

    First version of kotlin RecyclerView working

--- app/src/main/java/com/jeyabalaji/kotlin_recyclerview/MainActivity.kt
+++ app/src/main/java/com/jeyabalaji/kotlin_recyclerview/MainActivity.kt
@@ -3,7 +3,6 @@ package com.jeyabalaji.kotlin_recyclerview
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
@@ -13,22 +12,18 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
-//        Initializing the type of layout, here I have used LinearLayoutManager you can try GridLayoutManager
-//        Based on your requirement to allow vertical or horizontal scroll , you can change it in  LinearLayout.VERTICAL
-
-//        recyclerView.layoutManager = LinearLayoutManager(this, RecyclerView.VERTICAL, false)
         recyclerView.layoutManager = GridLayoutManager(this, 2)
 
 
-        val arr = intArrayOf(R.drawable.album1, R.drawable.album2, R.drawable.album3,
+        val arrayOfImages = intArrayOf(R.drawable.album1, R.drawable.album2, R.drawable.album3,
             R.drawable.album4, R.drawable.album5 ,R.drawable.album6, R.drawable.album7,
             R.drawable.album7)
 
         val dataList = ArrayList<Model>()
-        dataList.add(Model("Phone", 1, arr.get(0)))
-        dataList.add(Model("Watch", 2, arr.get(1)))
-        dataList.add(Model("Note", 3, arr.get(2)))
-        dataList.add(Model("Pin", 4, arr.get(3)))
+        dataList.add(Model("Phone", 1, arrayOfImages.get(0)))
+        dataList.add(Model("Watch", 2, arrayOfImages.get(1)))
+        dataList.add(Model("Note", 3, arrayOfImages.get(2)))
+        dataList.add(Model("Pin", 4, arrayOfImages.get(3)))
 
         val recyclerViewDataAdapter = DataAdapter(dataList,this)
 
